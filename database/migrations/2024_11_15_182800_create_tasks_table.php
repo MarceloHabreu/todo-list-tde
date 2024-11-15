a40f981d4520adfeb7754f2d34fc7375ed7758cc
MarceloHabreu <marcelohenriqueas17@gmail.com>
2024-11-15 15:32:36 -0300

    tables-created

---
+++ database/migrations/2024_11_15_182800_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamp('created_at')->useCurrent(); // data de criacao
+            $table->date('due_date')->nullable(); // data de vencimento
+            $table->string('status')->default('Pendente');
+            $table->uuid('user_id');
+            $table->uuid('category_id')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
